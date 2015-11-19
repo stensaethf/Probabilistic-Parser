43f9a7baa035c450c0d3568aa93e6b5dbcd024b6
Frederik Stensaeth <stensaethf@carleton.edu>
2015-11-19 16:44:01 -0600

    counts.py testing done

--- counts.py
+++ counts.py
@@ -30,15 +30,16 @@ def getCounts(counts_dict, root):
 		child_list = []
 		for child_node in root.children:
 			child_list.append(child_node.value)
+		child_str = ' '.join(child_list)
 
 		# If the grammar rule has been seen before we increment the count, if
 		# not we set the count to 1.
-		if child_list in counts_dict[root.value]:
+		if child_str in counts_dict[root.value]:
 			# Grammar rule seen before.
-			counts_dict[root.value][child_list] += 1
+			counts_dict[root.value][child_str] += 1
 		else:
 			# Grammar rule not seen before.
-			counts_dict[root.value][child_list] = 1
+			counts_dict[root.value][child_str] = 1
 
 		# Calls getCounts on each child in order to consider the entire tree.
 		for child_node in root.children:
@@ -73,6 +74,23 @@ def getProbabilities(counts_dict):
 			del prob_dict[lhs]
 
 	return prob_dict
+
+# # TEST
+# def main():
+# 	d = {}
+# 	d['a'] = {}
+# 	d['b'] = {}
+# 	d['a'][0] = 10
+# 	d['a']['aa aa'] = 5
+# 	d['a']['bb'] = 6
+# 	d['d'] = {}
+
+# 	prob = getProbabilities(d)
+
+# 	print(prob)
+
+# if __name__ == '__main__':
+# 	main()
 
 
 
