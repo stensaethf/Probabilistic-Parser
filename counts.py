20d5c569a1c12de52fc353d4ad2a0739f993b470
Frederik Stensaeth <stensaethf@carleton.edu>
2015-11-19 15:41:58 -0600

    count.py added

---
+++ counts.py
@@ -0,0 +1,23 @@
+# node
+# -> node.children -> list
+# first one is left child
+
+# end:
+# node.children == []
+
+# node.value
+
+def getCounts(counts_dict, root):
+	"""
+	getCounts() xx
+
+	@params: xx
+	@return: xx
+	"""
+
+	if root.value in counts_dict:
+
+
+
+
+	return counts_dict
