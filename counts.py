4698cacecbb7621e20bac74e912631ac56bfae87
Frederik Stensaeth <stensaethf@carleton.edu>
2015-11-19 16:18:45 -0600

    bug fix in getCounts()

--- counts.py
+++ counts.py
@@ -12,18 +12,20 @@ def getCounts(counts_dict, root):
 	@params: counts dictionary and tree (root node).
 	@return: updated counts dictionary.
 	"""
-	# If the root has been seen before we increment the count, if not we
-	# set the count to 1.
-	if root.value in counts_dict:
-		# Root seen before.
-		counts_dict[0] += 1
-	else:
-		# Root not seen before.
-		counts_dict[root.value] = {}
-		counts_dict[root.value][0] = 1
-
-	# If the root has children we also want to count these.
+	# If the root is a terminal value we do not do anything.
 	if root.children != []:
+		# If the root has been seen before we increment the count, if not we
+		# set the count to 1.
+		if root.value in counts_dict:
+			# Root seen before.
+			counts_dict[root.value][0] += 1
+		else:
+			# Root not seen before.
+			counts_dict[root.value] = {}
+			counts_dict[root.value][0] = 1
+
+		# # If the root has children we also want to count these.
+		# if root.children != []:
 		# Gets the list of children (their values).
 		child_list = []
 		for child_node in root.children:
@@ -66,6 +68,9 @@ def getProbabilities(counts_dict):
 				# the grammar rule.
 				prob = counts_dict[lhs][rhs] / counts_dict[lhs][0]
 				prob_dict[lhs][rhs] = prob
+
+		if prob_dict[lhs] == {}:
+			del prob_dict[lhs]
 
 	return prob_dict
 
