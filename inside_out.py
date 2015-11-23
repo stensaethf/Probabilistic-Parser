83cbddbd2801a42437cb470264ce6ff950487fb6
Frederik Stensaeth <stensaethf@carleton.edu>
2015-11-23 02:08:13 -0600

    alpha/beta does not crash, however, need to investigate

--- inside_out.py
+++ inside_out.py
@@ -43,8 +43,8 @@ def potential(tree, grammar):
 					# check = True
 		# child_list = []
 		# for child in root.children:
-		# 	child_list.append(child.value)
-		# 	potential += potential(child, grammar)
+		#   child_list.append(child.value)
+		#   potential += potential(child, grammar)
 
 		# prob = grammar.rules[root.value][' '.join(child_list)]
 		# print(check)
@@ -52,73 +52,76 @@ def potential(tree, grammar):
 
 	return pot
 
-def alpha(sentence, grammar, trees):
+def getAlpha(sentence, grammar, trees):
 	"""
 	calculateInsideProbabilities() xx
 
 	@params: xx
 	@return: xx
 	"""
-    n = len(sentence)
-    
-    alpha = {lhs: [[0]*n]*n for lhs in g.non_terminals}
-    
-    #BASE CASE
-    for lhs in grammar.non_terminals:
-        for i in range(n):
-            word = sentence[i]
-            if word in grammar.TR and lhs in grammar.TR[word]:
-                alpha[lhs][i][i] = grammar.TR[word][lhs].prob
-            else:
-                alpha[lhs][i][i] = 0
-    
-    for lhs in grammar.non_terminals:
-		for rule in rules[lhs].values():
+	n = len(sentence)
+	
+	alpha = {lhs: [[0]*n]*n for lhs in grammar.non_terminals}
+	
+	#BASE CASE
+	for lhs in grammar.non_terminals:
+		for i in range(n):
+			word = sentence[i]
+			if word in grammar.TR and lhs in grammar.TR[word]:
+				alpha[lhs][i][i] = grammar.TR[word][lhs].prob
+			else:
+				alpha[lhs][i][i] = 0
+	
+	for lhs in grammar.NR:
+		for rule in grammar.NR[lhs].values():
 			for i in range(n - 1):
 				for j in range(1, n):
 					for k in range(i+1, j):
 						prod = 1
-                        prod *= rule.prob
-                        prod *= alpha[rule.rhs[0]][i][k]
-                        prod *= alpha[rule.rhs[1]][k+1][j]
-                        alpha[lhs][i][j] += prod
+						prod *= rule.prob
+						prod *= alpha[rule.rhs[0]][i][k]
+						prod *= alpha[rule.rhs[1]][k+1][j]
+						alpha[lhs][i][j] += prod
 
 	return alpha
 
-def beta(sentence, grammar, trees):
-    beta = {lhs: [[0]*n]*n for lhs in g.non_terminals}
-    n = len(sentence)
-    
-    beta[grammar.start_symbol][0][n-1]
-    
-    for lhs in grammar.non_terminals:
-        for rule in rules[lhs].values():
-            rrhs = '|'.join(rule.rhs[::-1])
-            if rrhs not in rules[lhs]:
-                continue
-            
-            r_rule = rules[lhs][rrhs]
-            
-            for i in range(n-1):
-                for j in range(1, n):
-                    if i==0 and j==n-1:
-                        continue
-                        
-                    for k in range(i):
-                        prod = 1
-                        prod *= rule.prob
-                        prod *= alpha[rule.rhs[0]][k][i-1]
-                        prod *= beta[lhs][k][j]
-                        beta[rule.rhs[1]][i][j] += prod
-                        
-                    for k in range(j+1, n):
-                        prod = 1
-                        prod *= r_rule.prob
-                        prod *= alpha[r.rhs[0][i][k]]
-                        prod *= beta[lhs][i][k]
-                        beta[rule.rhs[1]][i][j] += prod
-                        
-    return beta
+def getBeta(sentence, grammar, trees, alpha):
+	n = len(sentence)
+	beta = {lhs: [[0]*n]*n for lhs in grammar.non_terminals}
+	
+	beta[grammar.start_symbol][0][n-1]
+	
+	for lhs in grammar.NR:
+		for rule in grammar.NR[lhs].values():
+			rrhs = '|'.join(rule.rhs[::-1])
+			if rrhs not in grammar.NR[lhs]:
+				continue
+			
+			r_rule = grammar.NR[lhs][rrhs]
+			
+			for i in range(n-1):
+				for j in range(1, n):
+					if i==0 and j==(n-1):
+						continue
+						
+					for k in range(i):
+						prod = 1
+						prod *= rule.prob
+						prod *= alpha[rule.rhs[0]][k][i-1]
+						prod *= beta[lhs][k][j]
+						beta[rule.rhs[1]][i][j] += prod
+						
+					for k in range(j+1, n):
+						prod = 1
+						prod *= r_rule.prob
+						print(i)
+						print(k)
+						print(alpha[rule.rhs[0]])
+						prod *= alpha[rule.rhs[0]][i][k]
+						prod *= beta[lhs][i][k]
+						beta[rule.rhs[1]][i][j] += prod
+						
+	return beta
 	
 def insideOutside(sentence, grammar):
 	"""
@@ -127,12 +130,12 @@ def insideOutside(sentence, grammar):
 	@params: xx
 	@return: xx
 	"""
-    
-    print 'Parsing'
-    trees = cky.cky(grammar, sentence)
-    print 'Inside'
-	inside = alpha(sentence, grammar, trees)
-    print 'Outside'
-	outside = beta(sentence, grammar, trees)
+	
+	print 'Parsing'
+	trees = cky.cky(grammar, sentence)
+	print 'Inside'
+	inside = getAlpha(sentence, grammar, trees)
+	print 'Outside'
+	outside = getBeta(sentence, grammar, trees, inside)
 
 	return None
