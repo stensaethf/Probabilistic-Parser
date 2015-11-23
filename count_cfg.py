8d251bbfbe1be3e2fb23de23dae3e81b8a480804
callahanp <callahanp@cmc306-16.mathcs.carleton.edu>
2015-11-23 02:55:14 -0600

    insideOutside ready to test

--- count_cfg.py
+++ count_cfg.py
@@ -57,6 +57,8 @@ def main():
     g.write(open('cfg', 'wb'))
     g.convertToCNF()
     
+    for lhs in g.NR:
+        print sum([x.prob for x in g.NR[lhs].values()])
     
     print 'Parsing Sentence'
     words = ['He', 'glowered', 'down', 'at', 'her']
